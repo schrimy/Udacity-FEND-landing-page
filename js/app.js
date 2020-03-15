b136cffbbfed6316fbf17a0b60fca763e0349c05
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-03-15 17:01:04 +0000

    add function to create nav elements and add to nav bar
    create a function that grabs all section tags and passes them to a 
    function that loops through each one creating a styled list item. Then 
    attach each to a frag which is returned. Then is attached to the nav 
    bar.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,57 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
-
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
+function createNavItems(sections) {
 
+  const navFrag = document.createDocumentFragment();
+
+  for (const section of sections) {
+    let navItem = document.createElement('li');
+    navItem.innerText = section.dataset.nav;
+    navItem.className = 'menu__link';
+
+    navFrag.appendChild(navItem);
+  }
+
+  return navFrag;
+
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+const navBar = document.querySelector('#navbar__list');
+const pageSections = document.querySelectorAll('section');
 
+const navContent = createNavItems(pageSections);
+navBar.appendChild(navContent);
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,13 +62,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
